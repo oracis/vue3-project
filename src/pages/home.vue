5aa740142de2035683981b248ed45a539d8f5d6d
robin zhou <robin_zhou@epam.com>
2025-02-24 23:40:07 +0800

    initialize project with router and pinia

---
+++ src/pages/home.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { useUserStore } from '../store/user';
+
+const user = useUserStore();
+</script>
+
+<template>
+    <span>{{ user.token }}</span>
+</template>
+
+<style scoped></style>
