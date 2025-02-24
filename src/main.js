bedd3bb17ba35ecb2380ea558cb58503e4f9ff18
robin zhou <robin_zhou@epam.com>
2025-02-25 00:34:14 +0800

    add element-plus and tailwindcss

--- src/main.js
+++ src/main.js
@@ -3,9 +3,18 @@ import './style.css';
 import App from './App.vue';
 import router from './router/router';
 import { createPinia } from 'pinia';
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css';
+import './tailwindcss.css';
+import * as EmementPlusIconsVue from '@element-plus/icons-vue';
 
 const pinia = createPinia();
 
 const app = createApp(App);
-app.use(router).use(pinia);
+
+Object.keys(EmementPlusIconsVue).forEach((key) => {
+  app.component(key, EmementPlusIconsVue[key]);
+});
+
+app.use(router).use(pinia).use(ElementPlus);
 app.mount('#app');
--- src/pages/home.vue
+++ src/pages/home.vue
@@ -5,7 +5,9 @@ const user = useUserStore();
 </script>
 
 <template>
-    <span>{{ user.token }}</span>
+    <span class="text-6xl text-red-500">{{ user.token }}</span>
+    <el-button type="primary">Click</el-button>
+    <el-icon><House /></el-icon>
 </template>
 
 <style scoped></style>
